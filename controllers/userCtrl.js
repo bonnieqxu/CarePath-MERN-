c0082646e0b07d4734919b6b40f21768d128016e
Bonnie Xu <bonnieqxu@gmail.com>
2024-08-18 17:35:57 +1200

    login and registration

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/userModels");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //register callback
 const registerController = async (req, res) => {
@@ -26,6 +27,60 @@ const registerController = async (req, res) => {
     });
   }
 };
-const loginController = () => {};
 
-module.exports = { loginController, registerController };
+// login callback
+const loginController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(200)
+        .send({ message: "User Not Found", success: false });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res
+        .status(200)
+        .send({ message: "Invlid Email or Password", success: false });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.status(200).send({ message: "Login Successful", success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: `Error in Login CTRL ${error.message}` });
+  }
+};
+
+const authController = async (req, res) => {
+  try {
+    // const user = await userModel.findById({ _id: req.body.userId });
+    const user = await userModel.findOne({ _id: req.body.userId });
+    // user.password = undefined;
+    if (!user) {
+      return res.status(200).send({
+        message: "User not found",
+        success: false,
+      });
+    } else {
+      res.status(200).send({
+        success: true,
+        // data: user,
+        data: {
+          name: user.name,
+          email: user.email,
+        },
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Authorisation Error",
+      success: false,
+      error,
+    });
+  }
+};
+
+module.exports = { loginController, registerController, authController };
